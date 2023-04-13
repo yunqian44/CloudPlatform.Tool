1b5f2c93c539d88db8ad6846bbde6f7c61be6b29
yunqian44 <yunqian8@live.com>
2023-04-13 22:15:04 +0800

    Add Package Model

---
+++ CloudPlatform.Tool.WebApp/Models/Package.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudPlatform.Tool.WebApp.Models;
+
+public class Package
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Vsersion { get; set; }
+
+    public string PublishDate { get; set; }
+
+    public string PublishType { get; set; }
+
+    public string Description { get; set; }
+     
+}
