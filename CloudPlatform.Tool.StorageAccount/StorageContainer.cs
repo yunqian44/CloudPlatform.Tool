09f1979ffd1bea39fab5b3bcd1fd70857927eb15
yunqian44 <yunqian8@live.com>
2023-04-16 00:32:00 +0800

    StorageContainer Entity

---
+++ CloudPlatform.Tool.StorageAccount/StorageContainer.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Storage.Blobs.Models;
+
+namespace CloudPlatform.Tool.StorageAccount;
+
+public class StorageContainer
+{
+    public string Name { get; set; }
+
+    public string PublishType { get; set; }
+
+    public string Status { get; set; }
+
+    public string LastModifyTime { get; set; }
+}
