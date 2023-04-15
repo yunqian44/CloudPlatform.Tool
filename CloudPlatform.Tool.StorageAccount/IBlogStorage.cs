30d564dec4e7c918d2320ca9e761dd8912bf076f
yunqian44 <yunqian8@live.com>
2023-04-15 15:18:55 +0800

    Add Azure StorageAccount

---
+++ CloudPlatform.Tool.StorageAccount/IBlogStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace CloudPlatform.Tool.StorageAccount;
+
+public interface IBlogStorage
+{
+    string Name { get; }
+
+    Task<string> InsertAsync(string fileName, byte[] imageBytes);
+
+    Task<BlobInfo> GetAsync(string fileName);
+
+    Task DeleteAsync(string fileName);
+}
---
+++ CloudPlatform.Tool.StorageAccount/Providers/AzureBlobStorage.cs
@@ -0,0 +1,92 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Logging;
+
+namespace CloudPlatform.Tool.StorageAccount.Providers;
+
+public class AzureBlobStorage : IBlogStorage
+{
+    public string Name => nameof(AzureBlobStorage);
+
+    private readonly BlobContainerClient _container;
+
+    private readonly ILogger<AzureBlobStorage> _logger;
+
+    public AzureBlobStorage(ILogger<AzureBlobStorage> logger, AzureBlobConfiguration blobConfiguration)
+    {
+        _logger = logger;
+
+        _container = new(blobConfiguration.ConnectionString, blobConfiguration.ContainerName);
+
+        logger.LogInformation($"Created {nameof(AzureBlobStorage)} for account {_container.AccountName} on container {_container.Name}");
+    }
+
+    public async Task<string> InsertAsync(string fileName, byte[] imageBytes)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+        {
+            throw new ArgumentNullException(nameof(fileName));
+        }
+
+        _logger.LogInformation($"Uploading {fileName} to Azure Blob Storage.");
+
+
+        var blob = _container.GetBlobClient(fileName);
+
+        // Why .NET doesn't have MimeMapping.GetMimeMapping()
+        var blobHttpHeader = new BlobHttpHeaders();
+        var extension = Path.GetExtension(blob.Uri.AbsoluteUri);
+        blobHttpHeader.ContentType = extension.ToLower() switch
+        {
+            ".jpg" => "image/jpeg",
+            ".jpeg" => "image/jpeg",
+            ".png" => "image/png",
+            ".gif" => "image/gif",
+            _ => blobHttpHeader.ContentType
+        };
+
+        await using var fileStream = new MemoryStream(imageBytes);
+        var uploadedBlob = await blob.UploadAsync(fileStream, blobHttpHeader);
+
+        _logger.LogInformation($"Uploaded image file '{fileName}' to Azure Blob Storage, ETag '{uploadedBlob.Value.ETag}'. Yeah, the best cloud!");
+
+        return fileName;
+    }
+
+    public async Task DeleteAsync(string fileName)
+    {
+        await _container.DeleteBlobIfExistsAsync(fileName);
+    }
+
+    public async Task<BlobInfo> GetAsync(string fileName)
+    {
+        var blobClient = _container.GetBlobClient(fileName);
+        await using var memoryStream = new MemoryStream();
+        var extension = Path.GetExtension(fileName);
+        if (string.IsNullOrWhiteSpace(extension))
+        {
+            throw new ArgumentException("File extension is empty");
+        }
+
+        var existsTask = blobClient.ExistsAsync();
+        var downloadTask = blobClient.DownloadToAsync(memoryStream);
+
+        var exists = await existsTask;
+        if (!exists)
+        {
+            _logger.LogWarning($"Blob {fileName} not exist.");
+
+            // Can not throw FileNotFoundException,
+            // because hackers may request a large number of 404 images
+            // to flood .NET runtime with exceptions and take out the server
+            return null;
+        }
+
+        await downloadTask;
+        var arr = memoryStream.ToArray();
+
+        var fileType = extension.Replace(".", string.Empty);
+
+        return new BlobInfo(memoryStream, fileType);
+    }
+}
