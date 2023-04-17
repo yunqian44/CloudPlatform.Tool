8582492659b160b144a0c9171d3bb638d474fbac
yunqian44 <yunqian8@live.com>
2023-04-17 17:52:36 +0800

    Add BlobStorage Model

---
+++ CloudPlatform.Tool.StorageAccount/StorageBlob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudPlatform.Tool.StorageAccount
+{
+    public class StorageBlob: StorageContainer
+    {
+        public string Name { get; set; }
+        public string ContainerName { get; set; }
+
+        public string PublishType { get; set; }
+
+        public string Status { get; set; }
+
+        public string LastModifyTime { get; set; }
+
+        public string Url { get; set; }
+    }
+}
