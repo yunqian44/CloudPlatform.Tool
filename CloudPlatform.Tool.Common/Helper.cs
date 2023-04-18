09052d5af990f522dc7d0fe7346bb3974c5d617e
yunqian44 <yunqian8@live.com>
2023-04-18 11:28:00 +0800

    Add Common Class Lib

---
+++ CloudPlatform.Tool.Common/Helper.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudPlatform.Tool.Common
+{
+    public static class Helper
+    {
+        public static string FormatCopyright2Html(string copyrightCode)
+        {
+            if (string.IsNullOrWhiteSpace(copyrightCode))
+            {
+                return copyrightCode;
+            }
+
+            var result = copyrightCode.Replace("[c]", "&copy;")
+                .Replace("[year]", DateTime.UtcNow.Year.ToString());
+            return result;
+        }
+    }
+}
